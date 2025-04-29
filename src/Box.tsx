15178d49e85dc617711d48f8b0995627f0b5f212
Nibor <67055088+robbiebeast005@users.noreply.github.com>
2025-04-29 13:44:26 +0200

    change video to yt

--- src/Box.tsx
+++ src/Box.tsx
@@ -20,11 +20,13 @@ function Box({ values } : BoxProps) {
     <div className='box'>
         <h1>{values.title}</h1>
         <p className='opperator'><b>Opperator</b> {values.opperator}</p>
-        <video width="320" poster={""} controls>
-            <source 
-            src={values.video} 
-            type='video/mp4'/>
-        </video>
+        <iframe
+            width="320"
+            height="180"
+            src={`https://www.youtube-nocookie.com/embed/${values.video}?rel=0&modestbranding=1&controls=1&showinfo=0`}
+            title="YouTube video player"
+            allowFullScreen
+        />
         <p className='description'><b>Description</b></p>
         <p className='text'> <em>{values.description}</em></p>
     </div>
